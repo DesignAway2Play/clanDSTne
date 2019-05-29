093f07127761ab179dbf4de6e06edffc37f7698d
Timothy 2 <timothy2@Timothys-MacBook-Air-2.local>
2019-05-29 14:45:29 -0700

    entrance page and cookies added

--- public/javascripts/general.js
+++ public/javascripts/general.js
@@ -1,7 +1,28 @@
-var ageProofC;
+const heroActive = document.querySelector('.hero');
+const singleD = 1000 * 60 * 60 * 24;
+var bDate; 
+var initProf;
 
+document.querySelector('#bDateSub').addEventListener('click', bDateCheck);
 
+/*
 initSetCookie();
 function initSetCookie() {
-    document.cookie = ageProofC + "=" + false + ";" + expires + ";path=/";
+    document.cookie = "ageProofC" + "=" + false + ";";
+}
+*/
+
+function bDateCheck(){
+    bDate = document.querySelector('.cal').value; 
+    var dateDif = Math.abs (new Date(bDate) - new Date());
+    console.log(dateDif);
+    var daysOld = (dateDif / singleD);
+    console.log(daysOld);
+    if (daysOld < 7665) {
+        document.cookie = "ageProofC" + "=" + false + ";";
+        console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    }
+    else {
+        document.cookie = "ageProofC" + "=" + true + ";";
+    }
 }
